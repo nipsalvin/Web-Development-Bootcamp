3433501d014e398065e631cfd9baf7cb8eedc895
nipsalvin <amwaniki.am@gmail.com>
2024-10-15 12:54:56 +0300

    `JavaScript` initial

---
+++ Web Development Projects/14.1 JavaScript Intro/index.js
@@ -0,0 +1,58 @@
+alert('Hello World'); //Returns an alert to the user on the browser
+var firstName = prompt('What is your name?'); //Returns a prompt to the user on the browser
+alert('Hello ' + firstName); //Alerts the user with their name
+
+firstName.length; //Returns the length of the string
+firstName.toUpperCase(); //Changes the name to uppercase
+firstName.toLowerCase(); //Changes the name to lowercase
+firstName.charAt(0); //Returns the letter at index 0
+firstName.slice(0, 3); //Returns the letters from index 0 to 3
+firstName.slice(0, firstName.length); //Returns the letters from index 0 to the number of letters in the length of the string
+
+// Arithmetic Operations
+var a = 5;
+var b = 2;
+a + b; //Adds the numbers
+a - b; //Subtracts the numbers
+a * b; //Multiplies the numbers
+a / b; //Divides the numbers
+a % b; //Returns the remainder of the division
+3+5*2; //Rule of order of operations applies here
+(3+5)*2; //Multiplies the numbers 
+
+// Increment and Decrement Operators
+var x = 10;
+x++; //Increments x by 1 -> 11
+x--; //Decrements x by 1 -> 10
+++x; //Increments x by 1 -> 11
+--x; //Decrements x by 1 -> 10
+x+=2; //Increments x by 2 -> 12
+x-=2; //Decrements x by 2 -> 10
+x*=2; //Multiplies x by 2 -> 20
+x/=2; //Divides x by 2 -> 10
+x%=2; //Returns the remainder of the division of x by 2 -> 0
+
+    //
+    var a = 5;
+    var b = a++; // 'b' is assigned the value of 'a' and then 'a' is incremented
+    var c = ++a; // 'c' is incremented and assigned the value of 'a'
+    var d = --b; // 'd' is decremented and assigned the value of 'b'
+    b += 1; 
+
+// Functions
+function getMilk() {
+    console.log('Here is your milk!');
+}
+
+function getMoreMilk(quantity) {
+    console.log('Here is ' + quantity + ' more milk!');    
+}
+
+function lifeInWeeks(age) {
+        var expectancy = 90 - age
+        var days = expectancy * 365
+        var weeks = expectancy * 52
+        var months = expectancy * 12
+        console.log('You have ' + days + ' days, ' + weeks +' weeks,' + months + ' months left' );
+    }
+    
