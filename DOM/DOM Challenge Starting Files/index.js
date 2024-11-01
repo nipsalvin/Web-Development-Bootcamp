781435da8dd7dc86a1c1e1e89a583680e8fd439a
nipsalvin <amwaniki.am@gmail.com>
2024-11-01 06:49:45 +0300

    `Switch Statements`

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -116,3 +116,61 @@ function calculator(num1, num2, operator) {
 /// In action:
 console.log(calculator(2, 2, multiply))
 console.log(calculator(2, 2, add))
+
+// Switch Operators
+switch (operator) {
+    case 'add':
+        return num1 + num2
+    case 'subtract':
+        return num1 - num2
+    case 'multiply':
+        return num1 * num2
+    case 'divide':
+        return num1 / num2
+    default:
+        return 'Invalid operator'
+}
+
+var buttons = document.querySelectorAll('.drum');
+var numberOfDrums = buttons.length;
+for (var i = 0; i < numberOfDrums; i++) {
+    buttons[i].addEventListener('click', function(){
+        // alert(buttons[i] + 'I got clicked!');
+        console.log(this)
+        // // 'this' is the element that triggered the event
+        // this.style.color = "white";
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case 'w':
+                var tom1  = new Audio('sounds/tom-1.mp3');
+                tom1 .play();                
+                break;
+            case 'a':
+                var tom2  = new Audio('sounds/tom-2.mp3');
+                tom2 .play();                
+                break;
+            case 's':
+                var tom3  = new Audio('sounds/tom-3.mp3');
+                tom3 .play();                
+                break;
+            case 'd':
+                var tom4  = new Audio('sounds/tom-4.mp3');
+                tom4 .play();                
+                break;
+            case 'j':
+                var snare  = new Audio('sounds/snare.mp3');
+                snare .play();                
+                break;
+            case 'k':
+                var crash  = new Audio('sounds/crash.mp3');
+                crash .play();                
+                break;
+            case 'l':
+                var kick  = new Audio('sounds/kick-bass.mp3');
+                kick .play();                
+                break;
+            default:
+                break;
+        }
+    })
+}
