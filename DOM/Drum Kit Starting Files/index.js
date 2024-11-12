f1be59a432aa4f350aadb225c1de2e702e5c5f15
nipsalvin <amwaniki.am@gmail.com>
2024-11-12 06:54:24 +0300

    `Keyboard Event Listeners`

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -5,15 +5,15 @@ function handleClick() {
 }
 
 // Adding Event Listeners to multiple buttons
-/// Method 1
-// var buttons = document.querySelectorAll('.drum');
-// buttons.forEach((button) => {
-//     button.addEventListener('click', () => {
-//         alert(button.innerText + ' I got clicked!');
-//     })
-// })
+////// Method 1
+//// var buttons = document.querySelectorAll('.drum');
+//// buttons.forEach((button) => {
+////     button.addEventListener('click', () => {
+////         alert(button.innerText + ' I got clicked!');
+////     })
+//// })
 
-/// Method 2
+////// Method 2
 var buttons = document.querySelectorAll('.drum');
 var numberOfDrums = buttons.length;
 for (var i = 0; i < numberOfDrums; i++) {
