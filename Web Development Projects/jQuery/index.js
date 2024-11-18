9c5751432c4664d67d50f93a40f3079bb6b7c2ef
nipsalvin <amwaniki.am@gmail.com>
2024-11-18 06:54:26 +0300

    `Manipulating Styles with jQuery`

--- Web Development Projects/jQuery/index.js
+++ Web Development Projects/jQuery/index.js
@@ -9,8 +9,24 @@ $(document).ready(function() {
 
 // In jQuery, there is no difference between selecting 1 element and selecting all elements
 //// selecting all
-document.querySelectorAll('button')
-$('button')
+document.querySelectorAll('button');
+$('button');
 //// selecting 1
-document.querySelector('button')
-$('button')
+document.querySelector('button');
+$('button');
+
+$('h1').css('color', 'red'); // Passing 2 arguments is setting a property
+$('h1').css('color'); // Passing 1 argument is getting a property
+
+//Adding classes
+$('h1').addClass('big-title');
+// Removing classes
+$('h1').removeClass('big-title');
+// Toggling classes
+$('h1').toggleClass('big-title');
+
+// adding multiple classes
+$('h1').addClass('big-title margin-50'); // In the same quotes separated by a space
+
+// Checking if class exists
+$('h1').hasClass('big-title'); // Returns true or false
