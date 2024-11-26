590d1f8db54dfa82d956545cf0371836bd559e80
nipsalvin <amwaniki.am@gmail.com>
2024-11-26 07:24:31 +0300

    Progress on __Simon Game__     * Selecting random Number     * Adding to list     * Adding animation

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,16 @@
-const buttons = $('.btn');
+var gamePattern = [];
+var buttonColours = ['red' , 'blue', 'green', 'yellow'];
+
+function nextSequence () {
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColor =buttonColours[randomNumber];
+    gamePattern.push(randomChosenColor)
+    var button = $('#' + randomChosenColor)
+
+    // Adding animation and removing animation
+    button.addClass('pressed');
+    setTimeout(function () {
+        button.removeClass('pressed');
+    }, 100);
+    
+};
