0bf8020a6d00c2a60341b6f60b82e203383ea203
nipsalvin <amwaniki.am@gmail.com>
2024-11-28 20:08:09 +0300

    `Animation` to buttons. `EventListeners` on `Buttons` for clicks

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,15 +2,14 @@ var gamePattern = [];
 var buttonColours = ['red' , 'blue', 'green', 'yellow'];
 var userClickedPattern = [];
 
-
 $('.btn').click(function () {
             console.log(this);
             var userChosenColor = $(this).attr('id');
             userClickedPattern.push(userChosenColor);
             playSound(userChosenColor);
+            animatePress(userChosenColor);
         }
     )
-
 
 function nextSequence () {
     var randomNumber = Math.floor(Math.random() * 4)
@@ -20,32 +19,19 @@ function nextSequence () {
     // Animating fadeIn and Fadeout
     button.fadeIn(100).fadeOut(100).fadeIn(100);
     // Play sound
-    var audio = new Audio('sounds/' + randomChosenColor + '.mp3');
-    audio.play();
+    playSound(randomChosenColor);
     
 };
 
+function playSound(name) {
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
 
-function playSound(button) {
-    switch(button) {
-        case 'blue':
-            var blueAudio = new Audio('sounds/blue.mp3');
-            blueAudio.play();
-            break;
-        
-        case 'green':
-            var greenAudio = new Audio('sounds/green.mp3');
-            greenAudio.play();
-            break;
-        
-        case 'red':
-            var redAudio = new Audio('sounds/red.mp3');
-            redAudio.play();
-            break;
-        
-        case 'yellow':
-            var yellowAudio = new Audio('sounds/yellow.mp3');
-            yellowAudio.play();
-            break;
-    }
+function animatePress(currentColor) {
+    button = $('#' + currentColor)
+    button.addClass('pressed');
+    setTimeout(function () {
+        button.removeClass('pressed');
+    }, 100);
 }
