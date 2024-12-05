ad58706895738f04d835d9c0d3e2da0f6fcb9d8e
nipsalvin <amwaniki.am@gmail.com>
2024-12-05 07:14:51 +0300

    writting an `Express Server`

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,6 +2,11 @@
 import express from 'express';
 // Create app from express
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
 // Call method 'listen'
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
