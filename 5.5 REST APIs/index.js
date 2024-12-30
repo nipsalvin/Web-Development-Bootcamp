ec7ad0dba33cb506186cd6d5444967ba5d54976e
nipsalvin <amwaniki.am@gmail.com>
2024-12-30 08:30:18 +0300

    `POST` method done

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -28,6 +28,7 @@ app.get("/", (req, res) => {
 
 app.post("/get-secret", async (req, res) => {
   const searchId = req.body.id;
+  console.log(req.body);
   try {
     const result = await axios.get(API_URL + "/secrets/" + searchId, config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
@@ -38,6 +39,12 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  try {
+    const result = await axios.post(API_URL + "/secrets", req.body, config)
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
