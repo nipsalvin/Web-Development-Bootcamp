054ac192a92c91bd1fea5aabbb2bffd2af55a451
nipsalvin <amwaniki.am@gmail.com>
2024-12-30 08:30:39 +0300

    `Put` Method Done

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -50,6 +50,12 @@ app.post("/post-secret", async (req, res) => {
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  try {
+    const result = await axios.put(API_URL + "/secrets/" + searchId, req.body, config)
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
