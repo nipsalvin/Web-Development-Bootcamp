8902c7406c7a4b93f0eec37db4630d81d5c1073d
nipsalvin <amwaniki.am@gmail.com>
2025-01-07 05:58:01 +0300

    using `.env` and `dotenv`

--- 6.0 DIY API/solution.js
+++ 6.0 DIY API/solution.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
-const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
+const masterKey = process.env.MASTER_KEY;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
