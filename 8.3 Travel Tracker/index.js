ce3103e60407f9d62092e4d0e0b2787debab417b
nipsalvin <amwaniki.am@gmail.com>
2025-02-03 07:52:29 +0300

    Add endpoint to add visited countries based on user input

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -34,6 +34,27 @@ app.get("/", async (req, res) => {
 // If you end the database connection, the server will no longer be able to access the database
 // db.end();
 
+app.post("/add", async (req, res) => {
+  const input = req.body["country"].trim();
+  debugger
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = '%' || $1 || '%'",
+    [input]
+  );
+  console.log(`input is ${input}`);
+  console.log(`result is ${result.rows}`);
+
+  if (result.rows.length !== 0) {
+    const country_code = result.rows[0].country_code;
+    console.log(country_code)
+    await db.query(
+      "INSERT INTO visited_countries (country_code) VALUES($1)",
+      [country_code]
+    );
+    res.redirect("/");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
