60b126d23f37b6490b4b1c16ac7fa05d4648e5f3
nipsalvin <amwaniki.am@gmail.com>
2025-03-24 07:04:03 +0300

    Update database name to `book_notes` and implement book data retrieval from OpenLibrary API

--- 8.7 Capstone Project - Book Notes/index.js
+++ 8.7 Capstone Project - Book Notes/index.js
@@ -12,13 +12,22 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
+  database: "book_notes",
   password: process.env.POSTGRES_PW,
   port: 5432
 });
 db.connect();
 
-
+app.get("/", async (req, res) => {
+  try{
+    // TODO get book data from OpenLibrary API using inputted ISBN or name
+    const isbn = 9780385533225;
+    const url = "https://openlibrary.org/isbn/"+isbn+".json";
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { book_data: result });
+  } catch (error) {}
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
