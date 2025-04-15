cd684ec1e11026c36cfd599070ed94af7400614c
nipsalvin <amwaniki.am@gmail.com>
2025-04-15 07:27:11 +0300

    Add initial authentication project structure with user registration and login functionality
    - Created package.json with dependencies for Express, EJS, body-parser, and pg.
    - Added SQL script to create users table.
    - Implemented Express server in solution.js with routes for home, login, and registration.
    - Developed EJS views for home, login, registration, and secrets pages.
    - Included partials for header and footer in EJS views.
    - Styled application with CSS for better user experience.

---
+++ 9.1+Authentication+Lv.1/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
