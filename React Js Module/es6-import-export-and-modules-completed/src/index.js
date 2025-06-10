6ff6bb2374331e4894372d8a36f6d9bf90a5e5c9
nipsalvin <amwaniki.am@gmail.com>
2025-06-10 06:41:13 +0300

    Adding a method 2 to import

--- React Js Module/es6-import-export-and-modules-completed/src/index.js
+++ React Js Module/es6-import-export-and-modules-completed/src/index.js
@@ -10,3 +10,18 @@ ReactDOM.render(
   </ul>,
   document.getElementById("root")
 );
+
+
+// Method 2 ** Not Recomended**
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import * as pie from "./math.js";
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{pie.default}</li>
+//     <li>{pie.doublePi()}</li>
+//     <li>{pie.triplePi()}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
