a99bf1b1d82a0df090136c169174c677f0a0c001
nipsalvin <amwaniki.am@gmail.com>
2025-06-24 06:50:52 +0300

    Initialize mapping-components-practice project with essential files, components, and styling
    Main agenda for today was to practice Splitting up a large file/page into smaller components

---
+++ React Js Module/mapping-components-practice/src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🤦",
+    name: "Person Facepalming",
+    meaning:
+      "Represents a person facepalming, which is a gesture of frustration, embarrassment, or disbelief. It conveys a sense of exasperation, often used when someone feels that a situation is silly or when they realize they made a mistake. It's a way to express that you can't believe something happened or that you wish you could take back a comment or action“.",
+  },
+];
+
+export default emojipedia;
